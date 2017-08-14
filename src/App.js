ee249e5031cdec350889e856cd765066bbe8c484
Elizaveta Buraya <Elizaveta Buraya>
2017-08-14 04:20:46 +0300

    Implemented city randomizer

---
+++ src/App.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const pairListObjects = {
+ "adjectives": [ "large", "fabulous", "important", "public", "bad" ],
+ "cities": [ "Budapest", "Buenos Aires", "Cairo", "Minsk", "Cape Town" ]
+};
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      canGenerate: true,
+      generatedList: [],
+      history: [],
+      textAreaHeight : 1,
+    };
+    this.generatedListLength = 0;
+    this.handleGenerateClick = this.handleGenerateClick.bind(this);
+  }
+
+  componentDidMount() {
+    let generatedPairsList = [];
+
+    pairListObjects.adjectives.forEach((adjective) => {
+      pairListObjects.cities.forEach((city) => generatedPairsList.push(adjective + " " + city))
+    });
+
+    this.generatedListLength = generatedPairsList.length;
+    this.setState({
+      generatedList: generatedPairsList
+    });
+  }
+
+  handleGenerateClick() {
+    let generatedPairs = this.state.generatedList;
+    let generatedHistory = this.state.history;
+    let randomPosition = Math.floor(Math.random() * generatedPairs.length);
+    let randomPair = generatedPairs[randomPosition].charAt(0).toUpperCase() + generatedPairs[randomPosition].slice(1);
+
+    this.textInput.value = randomPair;
+    generatedHistory.push(randomPair);
+    generatedPairs.splice(randomPosition, 1);
+
+    this.setState ({
+      generatedList: generatedPairs,
+      history: generatedHistory
+    });
+
+    this.autoGrow();
+
+    if (this.state.generatedList.length === 0) {
+      this.setState ({
+        canGenerate: false
+      });
+    }
+  }
+
+  autoGrow() {
+    let textHeight = this.state.textAreaHeight + 1;
+
+    this.setState({
+      textAreaHeight: textHeight
+    });
+  }
+
+  render() {
+    const history = this.state.history;
+    let generatedHistory = (history.length !== 0)
+                              ? history.join().replace(/,/g,'\n')
+                              : null;
+
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>City randomizer</h2>
+        </div>
+        <p className="App-content">
+          <div className="button-block">
+            <button disabled={!this.state.canGenerate}
+                    onClick={this.handleGenerateClick}>Generate
+            </button>
+          </div>
+          <span>Generated pair:</span>
+          <input type="text"
+                 ref={(input) => {this.textInput = input;}} /><br/>
+          <textarea value = {(generatedHistory) ? generatedHistory : ""}
+                    cols="38"
+                    rows={this.state.textAreaHeight} /><br />
+          <span>Generated {history.length} out of {this.generatedListLength}</span>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
