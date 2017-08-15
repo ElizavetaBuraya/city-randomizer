cfd855ac55d374969d17959e8d0347992e46ce5c
Unknown <lizaburaya@gmail.com>
2017-08-15 14:10:40 +0300

    Added redux Provider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import Container from './containers/AppContainer';
+import rootReducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Container />
+    </Provider>,
+    document.getElementById('root'));
 registerServiceWorker();
