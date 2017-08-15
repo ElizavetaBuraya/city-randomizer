cf589f6a12c86777ebd02bda845fe3586dee34d8
Unknown <lizaburaya@gmail.com>
2017-08-15 14:11:20 +0300

    Created AppContainer

---
+++ src/containers/AppContainer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { generatePairs,
+    updateHistory,
+    updateTextAreaHeight,
+    toggleCanGenerate } from '../actions/actionCreators';
+import App from '../components/App';
+
+const mapStateToProps = (state) => {
+    return {state}
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        generatePairsList:
+            bindActionCreators(generatePairs, dispatch),
+        renderHistory:
+            bindActionCreators(updateHistory, dispatch),
+        setTextAreaHeight:
+            bindActionCreators(updateTextAreaHeight, dispatch),
+        toggleGenerate:
+            bindActionCreators(toggleCanGenerate, dispatch),
+    }
+};
+
+export default
+connect(mapStateToProps, mapDispatchToProps)(App)
